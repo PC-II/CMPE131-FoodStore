bf56f3cb7f2e6056f5fe481c63259d24d475da62
alexxluong <alex.x.luong@sjsu.edu>
2024-03-28 16:34:17 -0700

    added account page, added account-button action

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ loginBtn.addEventListener("click", () => {
     // open the login pop up screen
     alert("Login button was pressed!");
 });
+const acntBtn = document.querySelector("#account-button");
+acntBtn.addEventListener("click", () => {
+    // open the account pop up screen
+    alert("Account button was pressed!");
+    window.open("accountPage.html");
+    window.close("index.html");
+});
 
