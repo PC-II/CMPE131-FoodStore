9a8768a54313e5c8711122cb04c4838096b6970d
kevynmaynard <kevynpanda449@gmail.com>
2024-04-25 19:31:51 -0700

    Deleted

--- index.js
+++
@@ -1,6 +0,0 @@
-const loginBtn = document.querySelector("#login-button");
-loginBtn.addEventListener("click", () => {
-    // open the login pop up screen
-    alert("Login button was pressed!");
-});
-
