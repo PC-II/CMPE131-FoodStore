fb174b5fcfbb01515df5d4dc5f81d3d8c8e72d4a
RyanChapa05 <ryan.chapa@sjsu.edu>
2024-05-07 20:59:28 -0700

    updated tables

--- storeDatabse.sql
+++ storeDatabse.sql
@@ -24,12 +24,13 @@ CREATE TABLE employee_info (
 );
 
 CREATE TABLE store_inventory (
-    item_id INT PRIMARY KEY,
+    item_id INT AUTO_INCREMENT PRIMARY KEY,
     item_name VARCHAR(255) NOT NULL,
-    item_category VARCHAR(255) NOT NULL,
+    item_price VARCHAR(255) NOT NULL,
+    item_weight VARCHAR(255) NOT NULL,
     item_quantity INT NOT NULL,
-    item_price DECIMAL(10, 2) NOT NULL,
-    item_weight DECIMAL(10, 2) NOT NULL
+    item_category VARCHAR(255) NOT NULL,
+    item_image  VARCHAR(255)
 );
 
 CREATE TABLE orders_history (
@@ -41,4 +42,37 @@ CREATE TABLE orders_history (
     address VARCHAR(255) NOT NULL,
     date_time DATETIME NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES customer_info(customer_id)
+);
+
+CREATE TABLE cart (
+  id int(255) NOT NULL,
+  name varchar(255) NOT NULL,
+  price varchar(255) NOT NULL,
+  weight varchar(255) NOT NULL,
+  quantity int(255) NOT NULL,
+  image varchar(255) NOT NULL,
+  category varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE messages (
+  Name varchar(250) NOT NULL,
+  Email varchar(250) NOT NULL,
+  Subject varchar(250) NOT NULL,
+  Message longtext NOT NULL
+) ;
+
+CREATE TABLE orders_history (
+  order_id INT AUTO_INCREMENT,
+  customer_id INT,
+  name VARCHAR(255),
+  number VARCHAR(255),
+  email VARCHAR(255),
+  street_name VARCHAR(255),
+  apartment_number VARCHAR(255),
+  city VARCHAR(255),
+  state VARCHAR(255),
+  zipcode VARCHAR(255),
+  total_product INT,
+  total_price VARCHAR(255),
+  PRIMARY KEY (order_id)
 );
